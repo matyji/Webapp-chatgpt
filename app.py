3f9aa8cc9f066ddd8d939deca4bb7c9c4d9c5d98
matyji <mathias.gun06@gmail.com>
2024-03-07 16:19:44 +0100

    Back-end: ajout des requêtes des messages via GPT

--- app.py
+++ app.py
@@ -1,10 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from GPT_requete import send_message
+import os
+import openai
+from openai import OpenAI
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+openai.api_key = "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"
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
+@app.route('/get_template/<template_name>')
+def get_template(template_name):
+    return render_template(f"{template_name}.html")
+
+
+
+@app.route('/envoyerRequete', methods=['POST'])
+async def send_request_to_openai():
+    # Exemple de récupération de données de la requête, ajustez selon vos besoins
+    data = request.json
+    user_message = data.get("message", "")
+
+    response = openai.completions.create(
+    model="gpt-3.5-turbo-instruct",
+    prompt=user_message,
+    max_tokens=300,
+    )
+    return jsonify({"reponse" : response.choices[0].text.strip()})
+
+
+@socketio.on('send_request')
+def handle_send_request(data):
+    user_message = data['message']
+    stream = openai.completions.create(
+        model="gpt-3.5-turbo-instruct",
+        prompt=user_message,
+        max_tokens=300,
+        stream=True
+    )
+    for response in stream:
+        socketio.emit('response', {'response': response.choices[0].text.strip()})
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
--- static/js/Chat.js
+++ static/js/Chat.js
@@ -14,3 +14,86 @@ document.addEventListener('DOMContentLoaded', function() {
     // Vérifie l'état initial au chargement de la page
     toggleButtonState();
 });
+
+function sendMessage() {
+    let sendButton = document.getElementById('send-button');
+    let userInput = document.getElementById('message-input').value;
+
+    // Vérifie si le bouton est désactivé
+    if (sendButton.disabled) {
+        // Le bouton est désactivé, donc ne rien faire (sortir de la fonction)
+        console.log('Le bouton est désactivé.');
+        return;
+    }else{
+        document.getElementById('message-input').value = null;
+        createMessageUserFromTemplate("message-user", userInput)
+        envoyerRequete(userInput);
+    }
+}
+
+
+document.getElementById('send-button').addEventListener('click', sendMessage);
+
+
+function createMessageUserFromTemplate(templateName, messageContent) {
+    fetch(`/get_template/${templateName}`)
+        .then(response => response.text())
+        .then(template => {
+            // Création d'un élément temporaire pour contenir le template HTML
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = template;
+            
+            // Mise à jour des parties variables du template
+            tempDiv.querySelector('.timestamp').textContent = new Date().toLocaleTimeString();
+            tempDiv.querySelector('.message-content-user').textContent = messageContent;
+            // Insérer le nouveau message dans le DOM
+            const messagesContainer = document.querySelector('.messages-container');
+            if (!messagesContainer) {
+                console.log('Le conteneur de messages n\'a pas été trouvé.');
+            } else {
+                messagesContainer.appendChild(tempDiv);
+            }
+        })
+        .catch(error => console.error('Error loading the template:', error));
+}
+
+function envoyerRequete(userInput) {
+    fetch('/envoyerRequete', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message: userInput }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Réponse reçue:', data);
+        reponse = data["reponse"]
+        createMessageAssistantFromTemplate("message-assistant", reponse)
+    })
+    .catch((error) => {
+        console.error('Erreur:', error);
+    });
+}
+
+function createMessageAssistantFromTemplate(templateName, reponse) {
+    fetch(`/get_template/${templateName}`)
+        .then(response => response.text())
+        .then(template => {
+            // Création d'un élément temporaire pour contenir le template HTML
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = template;
+            
+            // Mise à jour des parties variables du template
+            tempDiv.querySelector('.timestamp').textContent = new Date().toLocaleTimeString();
+            tempDiv.querySelector('.message-assistant-content').textContent = reponse;
+            // Insérer le nouveau message dans le DOM
+            const messagesContainer = document.querySelector('.messages-container');
+            if (!messagesContainer) {
+                console.log('Le conteneur de messages n\'a pas été trouvé.');
+            } else {
+                messagesContainer.appendChild(tempDiv);
+            }
+        })
+        .catch(error => console.error('Error loading the template:', error));
+}
